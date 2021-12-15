98e79819c8597a3eb98eec0efbf53a263b8c57a4
williamvega1393 <william.vega@uptc.edu.co>
2021-12-15 11:49:56 -0500

    config manifest and jcombobox and joptionpane

--- src/Views/HomeView.java
+++ src/Views/HomeView.java
@@ -18,6 +18,7 @@ public class HomeView extends JFrame implements ActionListener {
     private JButton buttonSub;
 
     private HomeController homeController;
+    private JComboBox jComboBox;
 
     public HomeView() {
         setLayout(null);
@@ -48,6 +49,15 @@ public class HomeView extends JFrame implements ActionListener {
         buttonSub.setBounds(100, 120, 70, 30);
         buttonSub.addActionListener(this);
         this.add(buttonSub);
+
+        jComboBox = new JComboBox<Integer>();
+        for (int i = 1871; i <= 2021; i++) {
+            jComboBox.addItem(i);
+        }
+        jComboBox.setBounds(100, 160, 70, 30);
+        jComboBox.addActionListener(this);
+        jComboBox.setSelectedItem(2000);
+        this.add(jComboBox);
     }
 
     @Override
@@ -63,6 +73,17 @@ public class HomeView extends JFrame implements ActionListener {
             } catch (NumberFormatException numberFormatException) {
                 labelError.setText("Valor no valido, ingresa un número entero");
             }
+
+            int res = JOptionPane.showConfirmDialog(this, "Prueba", "Titulo de prueba", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+            if (res == JOptionPane.NO_OPTION) {
+                labelOut.setText("No");
+            }
+            if (res == JOptionPane.YES_OPTION) {
+                labelOut.setText("Si");
+            }
+            if (res == JOptionPane.CLOSED_OPTION) {
+                labelOut.setText("Dialogo cerrado");
+            }
         }
         if (e.getSource() == buttonSub) {
             try {
@@ -73,6 +94,17 @@ public class HomeView extends JFrame implements ActionListener {
                 labelOut.setText(homeController.getRes() + "");
             } catch (NumberFormatException numberFormatException) {
                 labelError.setText("Valor no valido, ingresa un número entero");
+            }
+
+            jComboBox.removeAllItems();
+            for (int i = 0; i < 31; i++) {
+                jComboBox.addItem(i + 1);
+            }
+        }
+
+        if (e.getSource() == jComboBox) {
+            if (jComboBox.getSelectedItem() != null) {
+                labelIn.setText(jComboBox.getSelectedItem().toString());
             }
         }
     }
